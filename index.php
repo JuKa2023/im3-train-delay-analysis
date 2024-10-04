25d4d7d6691811f36ba0b25abcbd85fc42e78136
Julie <150014327+JuKa2023@users.noreply.github.com>
2024-10-04 18:16:28 +0200

    Initial set up

--- index.php
+++ index.php
@@ -1,9 +1,53 @@
 <?php
-// A simple PHP demo file for testing
-echo "<h1>PHP Demo Index</h1>";
-echo "<p>This is a simple PHP file to test your repository setup.</p>";
 
-// Display the current server time
-date_default_timezone_set("UTC");
-echo "<p>Current server time: " . date("Y-m-d H:i:s") . " UTC</p>";
+$test = "hello world";
+
+echo $test;
+
 ?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    <?php echo $test ?>
+</h1>
+
+<div>
+    <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [ '<?php echo $test ?>', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+
+</body>
+</html>
